53b6dcf6d1d88177f88c2add2806d43af022d4fc
KenMwaura1 <kemwaura@gmail.com>
2021-12-06 22:41:25 +0300

    created close and error function for websocket

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import websocket as wb
 
 BINANCE_SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
-ws = wb.WebSocketApp("")
 
 
 def on_open(ws):
@@ -9,8 +8,17 @@ def on_open(ws):
     print("connected")
 
 
+def on_close(ws):
+    print("closed connection")
+
+
+def on_error(ws, error):
+    print(error)
+
+
 def on_message(ws, message):
     print(message)
 
 
+ws = wb.WebSocketApp(BINANCE_SOCKET, on_open=on_open, on_close=on_close, on_error=on_error, on_message=on_message)
 ws.run_forever()
