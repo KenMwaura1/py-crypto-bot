aabac8271260302f420d0e457cf6f0f1fb3cdd95
KenMwaura1 <kemwaura@gmail.com>
2021-12-06 23:04:34 +0300

    updated the on_message function to track close price

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import websocket as wb
+from pprint import pprint
+import json
 
 BINANCE_SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
 
 def on_open(ws):
-    ws.send("{'event':'addChannel','channel':'ethusdt@kline_1m'}")
+    # ws.send("{'event':'addChannel','channel':'ethusdt@kline_1m'}")
     print("connected")
 
 
@@ -17,7 +19,17 @@ def on_error(ws, error):
 
 
 def on_message(ws, message):
-    print(message)
+    message = json.loads(message)
+    pprint(message)
+    candle = message['k']
+    is_candle_closed = candle['x']
+    if is_candle_closed:
+        closed = candle['c']
+        open = candle['o']
+        high = candle['h']
+        low = candle['l']
+        volume = candle['v']
+        pprint(f"closed: {closed}")
 
 
 ws = wb.WebSocketApp(BINANCE_SOCKET, on_open=on_open, on_close=on_close, on_error=on_error, on_message=on_message)
