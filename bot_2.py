057092e638e71aa90071fcfbd589d77898f61704
KenMwaura1 <kemwaura@gmail.com>
2024-01-24 09:32:01 +0300

    Add bot_2.py with WebSocket functionality

---
+++ bot_2.py
@@ -0,0 +1,119 @@
+import os
+import websocket as wb
+from pprint import pprint
+import json
+import redis
+import time
+# import talib
+# import numpy as np
+from binance.client import Client
+from binance.enums import *
+from dotenv import load_dotenv
+from datetime import datetime
+from database_insert import create_table
+from base_sql import Session
+from price_data_sql import CryptoPrice
+
+load_dotenv()
+
+# connect to Redis
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+# this functions creates the table if it does not exist
+create_table()
+
+# create a session
+session = Session()
+
+BINANCE_SOCKET = "wss://stream.binance.com:9443/stream?streams=ethusdt@kline_3m/btcusdt@kline_3m"
+B_S = "wss://stream.binance.com:9443/stream?streams=btcusdt@aggTrade/btcusdt@depth"
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = "ETHUSD"
+TRADE_SIZE = 0.05
+closed_prices = []
+in_position = False
+
+API_KEY = os.environ.get("API_KEY")
+API_SECRET = os.environ.get("API_SECRET")
+client = Client(API_KEY, API_SECRET, tld='us')
+
+
+def order(side, size, order_type=ORDER_TYPE_MARKET, symbol=TRADE_SYMBOL):
+    # order_type = "MARKET" if side == "buy" else "LIMIT"
+    try:
+        order = client.create_order(
+            symbol=symbol,
+            side=side,
+            type=order_type,
+            quantity=size,
+        )
+        print(order)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def on_open(ws):
+    # ws.send("{'event':'addChannel','channel':'ethusdt@kline_1m'}")
+    print("connection opened")
+
+
+def on_close(ws):
+    print("closed connection")
+
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_message(ws, message):
+    message = json.loads(message)
+    pprint(message)
+    candle = message['k']
+    trade_symbol = message['s']
+    is_candle_closed = candle['x']
+    global closed_prices
+    if is_candle_closed:
+        symbol = candle['s']
+        closed = candle['c']
+        open = candle['o']
+        high = candle['h']
+        low = candle['l']
+        volume = candle['v']
+        pprint(f"closed: {closed}")
+        pprint(f"open: {open}")
+        pprint(f"high: {high}")
+        pprint(f"low: {low}")
+        pprint(f"volume: {volume}")
+        closed_prices.append(float(closed))
+        # create price entries
+        print(TRADE_SYMBOL)
+        crypto = CryptoPrice(crypto_name=symbol, open_price=open, close_price=closed,
+                             high_price=high, low_price=low, volume=volume, time=datetime.utcnow())
+        # print(crypto.time, crypto.crypto_name, crypto.close_price, crypto.open_price, crypto.volume,
+        # crypto.high_price, crypto.low_price)
+        session.add(crypto)
+        session.commit()
+        session.close()
+        # add message to Redis queue
+        message_data = {
+            "symbol": symbol,
+            "open_price": open,
+            "close_price": closed,
+            "high_price": high,
+            "low_price": low,
+            "volume": volume,
+            "time": str(crypto.time)
+        }
+        r.lpush("crypto", json.dumps(message_data))
+        print(closed_prices)
+
+
+# create a WebSocketApp instance
+ws = wb.WebSocketApp(BINANCE_SOCKET, on_open=on_open, on_close=on_close, on_error=on_error, on_message=on_message)
+
+# run the WebSocket connection in a separate thread
+ws.run_forever()
